bf4c4507ed76f61416ee4fb22cecd542cb47a2fa
Julien <qoyajunk@gmail.com>
2014-12-28 22:42:58 +0100

    Commit and Push alias

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-var channelName = 'mll254';
-
 $(document).ready(function() {
     output = '<li>'+'<iframe width="560" height="315" src="//www.youtube.com/embed/sbQQKI1Fwo4?rel=0" frameborder="0" allowfullscreen></iframe>'+'<li>';
     $('#results').append(output);
 );
-
-function getVids(pid){
