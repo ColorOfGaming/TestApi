711ebebb509f11a1bf2af7a03450840298c76c57
Julien <qoyajunk@gmail.com>
2014-12-28 22:40:19 +0100

    Commit and Push alias

--- js/script.js
+++ js/script.js
@@ -1,46 +1,8 @@
 var channelName = 'mll254';
 
 $(document).ready(function() {
-     $.get(
-          "https://www.googleapis.com/youtube/v3/channels",{
-               part: "contentDetails",
-               forUsername: channelName,
-               key: 'AIzaSyDZjnohKcV1yQhaTkp7Ss5c1zf4RtmMjS0'},
-               function (data){
-                    $.each(data.items, function(i, item){
-                         console.log(item);
-                         pid = item.contentDetails.relatedPlaylists.uploads;
-                         getVids(pid);
-                    })
-               }
-     );
+    output = '<li>'+'<iframe width="560" height="315" src="//www.youtube.com/embed/sbQQKI1Fwo4?rel=0" frameborder="0" allowfullscreen></iframe>'+'<li>';
+    $('#results').append(output);
+);
 
 function getVids(pid){
-
-output = '<li>'+'<iframe width="560" height="315" src="//www.youtube.com/embed/sbQQKI1Fwo4?rel=0" frameborder="0" allowfullscreen></iframe>'+'<li>';
-$('#results').append(output);
-
-$.get(
-"https://www.googleapis.com/youtube/v3/playlistItems",{
-           part: 'snippet',
-           maxResults: 10,
-           playlistId: pid,
-           key: 'AIzaSyDZjnohKcV1yQhaTkp7Ss5c1zf4RtmMjS0'},
-           function(data){
-                var output;
-                $.each(data.items, function(i, item){
-                     console.log(item);
-                     videTitle = item.snippet.title;
-                     
-                     output = '<li>'+'<iframe width="560" height="315" src="//www.youtube.com/embed/sbQQKI1Fwo4?rel=0" frameborder="0" allowfullscreen></iframe>'+'<li>';
-                     
-                     //Append to results listStyleType
-                     $('#results').append(output);
-                })
-           }
-);
-           
-        }
-});
-      
-       
