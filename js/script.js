26b7cdcf2d36eab5da68d88ffe6686e89783cc22
Julien <blah@blah.com>
2015-01-08 09:28:57 +0100

    dwa

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     "https://www.googleapis.com/youtube/v3/channels",{
       part: "contentDetails",
       forUsername: "qoyaqa",
-      key: 'AIzaSyBra4wZMz0gsWgsQZumMgv9Y4juFQVhHhs'
+      key: 'AIzaSyBrQ3kxIFYp1iA4N5wWQ-j96dbHQMrunnc'
     },
     function (data){
         $.each(data.items, function(i,item){
