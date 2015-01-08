d265147cf9d284f657a939b28a87b712b8f7b133
Julien <blah@blah.com>
2015-01-08 09:46:37 +0100

    dwa

--- js/script.js
+++ js/script.js
@@ -2,18 +2,14 @@ var channelName = 'TechGuyWeb';
 
 $(document).ready(function() {
   $.get(
-    "https://www.googleapis.com/youtube/v3/channels",{
+    "https://www.googleapis.com/youtube/v3/channels",
+    {
       part: "contentDetails",
       forUsername: "qoyaqa",
       key: "AIzaSyDSmV8UbLjqmWKXwlmQegP-YsY9hEkBWes"
-    } ,
+    },
     function (data){
-      $.each(
-        data.items, 
-        function(i,item){
-          console.log(item);
-        }
-      )
+      $.each(data.items,function(i,item){console.log(item);})
     }
   );
 });
