7c3c706c056c1f7b2bccba191f59d7f1c39849ea
Julien <blah@blah.com>
2015-01-08 08:57:58 +0100

    Test API console log

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+var channelName = 'TechGuyWeb';
+
 $(document).ready(function() {
-  videoelmt = '<li>'+'<iframe width="560" height="315" src="//www.youtube.com/embed/sbQQKI1Fwo4?rel=0" frameborder="0" allowfullscreen></iframe>'+'<li>';
-  document.getElementById("results").innerHTML = videoelmt;
-  $("#results").append(videoelmt);
-});
+  $.get(
+    "https://www.googleapis.com/youtube/v3/channels",{
+      part: "contentDetails",
+      forUsername: channelName,
+      key: 'AIzaSyBra4wZMz0gsWgsQZumMgv9Y4juFQVhHhs'
+    },
+    function (data){
+        $.each(data.items, function(){
+          console.log(item);
+      })
+    }
+  );
+}
