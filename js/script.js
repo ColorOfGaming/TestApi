0f55219de3770c27676195ba07bf40ac9762815e
Julien <blah@blah.com>
2015-01-08 09:37:03 +0100

    frrf

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     "https://www.googleapis.com/youtube/v3/channels",{
       part: "contentDetails",
       forUsername: "qoyaqa",
-      key: 'AIzaSyBrQ3kxIFYp1iA4N5wWQ-j96dbHQMrunnc'
+      key: "AIzaSyDSmV8UbLjqmWKXwlmQegP-YsY9hEkBWes"
     },
     function (data){
         $.each(data.items, function(i,item){
